11662dacd7959e7776bb59f8ca3da50c7aa67b32
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-11-29 15:44:31 -0500

    Added Ternary

--- Syntax Exercise/Program.cs
+++ Syntax Exercise/Program.cs
@@ -1,10 +1,20 @@
-﻿int answer = 4;
-string response;
-if (answer < 9)
-{
-    response = answer + " is less than nine";
-}
-else
-{
-    response = answer + " is greater than or equal to nine";
-}
+﻿//Syntax Assignment Nick Carlson
+
+//Orginal code
+
+//int answer = 4;
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+
+// New Code
+
+var answer = 4;
+var result = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is more than 9";
+Console.WriteLine(result);
